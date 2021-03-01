d92adf399221ca8970d1158e1084d92065ef8f71
Laurent Dezo <laurentdezo@gmail.com>
2021-03-01 11:23:25 -0500

    Add button that resets sketchpad and ask for number of rows

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const container = document.getElementById('container');
 const divArray = ['']
+const button = document.getElementById('resetButton');
+let rowNumber = 0;
 
 
 for (i = 0; i < (10000); i++) {
@@ -10,6 +12,14 @@ for (i = 0; i < (10000); i++) {
 
 divArray.forEach(div => div.addEventListener('mouseover', colorDiv));
 
+button.addEventListener('click', reset);
+
 function colorDiv(e) {
     e.target.classList.add('colored');
+}
+
+function reset() {
+    divArray.forEach(div => div.classList.remove('colored'));
+    rowNumber = prompt('How many rows and columns should the sketchpad have?', '10 to 100 is acceptable.');
+    console.log(rowNumber);
 }
